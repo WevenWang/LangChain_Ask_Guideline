5fe127e8533e23c34f67055f3a81ae9a7ea8117b
Yifan Wang <35350620+WevenWang@users.noreply.github.com>
2024-03-30 17:08:10 -0700

    change to chat interaction

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+from langchain_openai import ChatOpenAI
+
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 
 from utils import load_knowledge_base
 
@@ -19,22 +22,46 @@ def main():
     
     
     knowledge_base = load_knowledge_base()
+    st.session_state.retriever = knowledge_base.as_retriever()
     
     # show user input
-    user_question = st.text_input("Ask a question about your New Wave Guidelines:")
-    if user_question:
-      docs = knowledge_base.similarity_search(user_question)
+    # user_question = st.text_input("Ask a question about your New Wave Guidelines:")
+    # if user_question:
+    #   docs = knowledge_base.similarity_search(user_question)
       
-      llm = OpenAI()
-      chain = load_qa_chain(llm, chain_type="stuff")
-      with get_openai_callback() as cb:
-        response = chain.run(input_documents=docs, question=user_question)
-        print(cb)
+    #   llm = OpenAI()
+    #   chain = load_qa_chain(llm, chain_type="stuff")
+    #   with get_openai_callback() as cb:
+    #     response = chain.run(input_documents=docs, question=user_question)
+    #     print(cb)
           
-      st.write(response)
+    #   st.write(response)
 
+    # convert above to a chatbot with 
+  
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    #
+    for message in st.session_state.messages:
+        st.chat_message('human').write(message[0])
+        st.chat_message('ai').write(message[1])    
+    #
+    if query := st.chat_input():
+        st.chat_message("human").write(query)
+        response = query_llm(st.session_state.retriever, query)
+        st.chat_message("ai").write(response)
 
-
+def query_llm(retriever, query):
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(),
+        retriever=retriever,
+        return_source_documents=True,
+    )
+    result = qa_chain({'question': query, 'chat_history': st.session_state.messages})
+    result = result['answer']
+    st.session_state.messages.append((query, result))
+    return result
+   
 
 
 if __name__ == '__main__':
